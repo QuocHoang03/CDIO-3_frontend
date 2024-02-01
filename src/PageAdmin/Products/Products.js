5d1eccc2c325e5f3ade1c97995e278b7f0685ab9
PHAMQUOCHOANG\quoch <quochoang22603>
2024-02-01 12:58:40 +0700

    update

--- src/PageAdmin/Products/Products.js
+++ src/PageAdmin/Products/Products.js
@@ -5,10 +5,21 @@ import { IoMenuOutline, IoAddOutline } from "react-icons/io5";
 import { LuListTree } from "react-icons/lu";
 import { MdDeleteForever } from "react-icons/md";
 import { FaRegEdit } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const cx = classNames.bind(styles);
 
 const Order = () => {
+  const handleNewProduct = (e) => {
+    e.preventDefault();
+    toast("Chức năng đang phát triển");
+  };
+  const handleUpdate = () => {
+    toast("Chức năng đang phát triển");
+  };
+  const handleDelete = () => {
+    toast("Chức năng đang phát triển");
+  };
   const fakeDemoArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className={cx("wrapper")}>
@@ -18,7 +29,7 @@ const Order = () => {
           <span>Products</span>
         </div>
         <div className={cx("heading-right")}>
-          <button className={cx("btn-create")}>
+          <button className={cx("btn-create")} onClick={handleNewProduct}>
             <IoAddOutline className={cx("btn-icon")} />
             <span>New Product</span>
           </button>
@@ -26,40 +37,44 @@ const Order = () => {
       </h2>
       <div className={cx("container")}>
         <table className={cx("table-inner")}>
-          <tr className={cx("row-inner")}>
-            <th>
-              <div className={cx("th-content")}>
-                <LuListTree className={cx("th-content-icon")} />
-                ID
-              </div>
-            </th>
-            <th>Title</th>
-            <th>Published at</th>
-            <th>enabled</th>
-            <th>Action</th>
-          </tr>
-          {fakeDemoArray.map((index) => {
-            return (
-              <tr key={index} className={cx("row-inner")}>
-                <td>{index}</td>
-                <td>Phạm Quốc Hoàng</td>
-                <td>2024-11-1</td>
-                <td>
-                  <input className={cx("input-checkbox")} type="checkbox" checked={false} />
-                </td>
-                <td>
-                  <div className={cx("action")}>
-                    <div className={cx("action-upd")}>
-                      <FaRegEdit className={cx("action-upd-icon")} />
+          <thead>
+            <tr className={cx("row-inner")}>
+              <th>
+                <div className={cx("th-content")}>
+                  <LuListTree className={cx("th-content-icon")} />
+                  ID
+                </div>
+              </th>
+              <th>Title</th>
+              <th>Published at</th>
+              <th>enabled</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {fakeDemoArray.map((index) => {
+              return (
+                <tr key={index} className={cx("row-inner")}>
+                  <td>{index}</td>
+                  <td>Phạm Quốc Hoàng</td>
+                  <td>2024-11-1</td>
+                  <td>
+                    <input className={cx("input-checkbox")} type="checkbox" />
+                  </td>
+                  <td>
+                    <div className={cx("action")}>
+                      <div className={cx("action-upd")} onClick={handleUpdate}>
+                        <FaRegEdit className={cx("action-upd-icon")} />
+                      </div>
+                      <div className={cx("action-del")} onClick={handleDelete}>
+                        <MdDeleteForever className={cx("action-del-icon")} />
+                      </div>
                     </div>
-                    <div className={cx("action-del")}>
-                      <MdDeleteForever className={cx("action-del-icon")} />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </div>
